1f8dd52e7221f3e6150d3e0c163a019c36385269
Brian Cho <bcho0824@gmail.com>
2019-01-22 14:27:15 -0500

    onsterlegs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+tank_legs = Leg.create(name: "Tank Legs", weight: 581, power: 105, defense: 930, speed: 37)
+horse_legs = Leg.create(name: "Horse Legs", weight: 206, power: 304, defense: 536, speed: 174)
+rocket_legs = Leg.create(name: "Rocket Legs", weight: 380, power: 261, defense: 763, speed: 191)
+high_heel_legs = Leg.create(name: "High Heel Legs", weight: 143, power: 382, defense: 30, speed: 48)
