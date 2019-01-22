7993991836cef6ed7dbe5c4a944d0b9638ea5444
Arthur Torres <arthurtorres002@gmail.com>
2019-01-22 14:27:50 -0500

    add arm seeds

---
+++ db/schema.rb
@@ -0,0 +1,75 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_01_22_181243) do
+
+  create_table "arms", force: :cascade do |t|
+    t.string "name"
+    t.string "limg"
+    t.string "rimg"
+    t.integer "weight"
+    t.integer "power"
+    t.integer "defense"
+    t.integer "cuteness"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "heads", force: :cascade do |t|
+    t.string "name"
+    t.string "img"
+    t.integer "weight"
+    t.integer "power"
+    t.integer "defense"
+    t.integer "cuteness"
+    t.integer "intelligence"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "legs", force: :cascade do |t|
+    t.string "name"
+    t.string "img"
+    t.integer "weight"
+    t.integer "power"
+    t.integer "defense"
+    t.integer "speed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "monsters", force: :cascade do |t|
+    t.string "name"
+    t.integer "head_id"
+    t.integer "torso_id"
+    t.integer "leg_id"
+    t.integer "arm_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arm_id"], name: "index_monsters_on_arm_id"
+    t.index ["head_id"], name: "index_monsters_on_head_id"
+    t.index ["leg_id"], name: "index_monsters_on_leg_id"
+    t.index ["torso_id"], name: "index_monsters_on_torso_id"
+  end
+
+  create_table "torsos", force: :cascade do |t|
+    t.string "name"
+    t.string "img"
+    t.integer "weight"
+    t.integer "power"
+    t.integer "defense"
+    t.boolean "dadbod"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+flamethrower_arms = Arm.create(name: "Flamethrower Arms", weight: 115, power:391, defense: 370, cuteness: 8)
+bear_arms = Arm.create(name: "Bear Arms", weight: 107, power:245, defense: 521, cuteness: 15)
+gun_arms = Arm.create(name: "Gun Arms", weight: 100, power:35, defense: 420, cuteness: 19)
+hook_arms = Arm.create(name: "Hook Arms", weight: 88, power:235, defense: 343, cuteness: 6)
+lobster_arms = Arm.create(name: "Lobster Arms", weight: 94, power:186, defense: 920, cuteness: 13)
+shield_arms = Arm.create(name: "Shield Arms", weight: 136, power:77, defense: 950, cuteness: 5)
