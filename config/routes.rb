fbbab9d8aa10f579a28d4165e0c1e337a552a3b1
Arthur Torres <arthurtorres002@gmail.com>
2019-01-24 14:10:08 -0500

    add root rout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'monsters#index'
   get 'monsters/stats', to: 'monsters#stats', as: 'monsters_stats'
   resources :monsters
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
