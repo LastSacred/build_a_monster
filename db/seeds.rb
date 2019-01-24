c19e4fda416c6d261800fb25c5376b67d1fa6578
Arthur Torres <arthurtorres002@gmail.com>
2019-01-24 18:10:43 -0500

    update torso stats and dateability method

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,10 +24,10 @@ mustache_head = Head.create(name: "Mustache Head", img: "mustache_head.png", wei
 zombie_head = Head.create(name: "Zombie Head", img: "zombie_head.png", weight: 28, power: 82, defense: 3, cuteness: 14, intelligence: 1)
 # heart_eyes_head = Head.create(name: "Heart Eyes Head", img: "laser_eyes_head.png", weight: 35, power: 150, defense: 111, cuteness: 20, intelligence: 8)
 
-lumberjack_torso = Torso.create(name: "Lumberjack Torso", img: "lumberjack_torso.png", weight: 250, power: 0, defense: 695, dadbod: true)
-# turtle_shell_torso = Torso.create(name: "Turtle Shell Torso", img: "lumberjack_torso.png", weight: 410, power: 0, defense: 877, dadbod: false)
-iron_man_torso = Torso.create(name: "Iron Man Torso", img: "iron_man_torso.png", weight: 573, power: 278, defense: 900, dadbod: false)
-sumo_torso = Torso.create(name: "Sumo Torso", img: "sumo_torso.png", weight: 600, power: 103, defense: 708, dadbod: true)
+lumberjack_torso = Torso.create(name: "Lumberjack Torso", img: "lumberjack_torso.png", weight: 134, power: 10, defense: 648, dadbod: true)
+# turtle_shell_torso = Torso.create(name: "Turtle Shell Torso", img: "lumberjack_torso.png", weight: 162, power: 18, defense: 776, dadbod: false)
+iron_man_torso = Torso.create(name: "Iron Man Torso", img: "iron_man_torso.png", weight: 214, power: 208, defense: 792, dadbod: false)
+sumo_torso = Torso.create(name: "Sumo Torso", img: "sumo_torso.png", weight: 235, power: 101, defense: 692, dadbod: true)
 
 manbearpig = Monster.create(name: "Manbearpig", head: laser_eyes_head, torso: iron_man_torso, arm: bear_arms, leg: tank_legs)
 curmudgeon = Monster.create(name: "The Curmudgeon", head: zombie_head, torso: lumberjack_torso, arm: lobster_arms, leg: horse_legs)
